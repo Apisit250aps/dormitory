886eb1dbf0c1557238e05045ec8e818a59b26315
YUapisit <aps.apisit250@gmail.com>
2025-06-22 18:23:08 +0700

    Add initial data.json file for building dashboard with comprehensive project details
    - Introduced a new JSON file containing 68 entries related to project components.
    - Each entry includes fields such as id, header, type, status, target, limit, and reviewer.
    - Covers various aspects including narratives, technical content, planning, legal, and research.
    - Statuses include 'In Process' and 'Done' to reflect current progress on each component.

--- src/app/dashboard/building/page.tsx
+++ src/app/dashboard/building/page.tsx
@@ -1,9 +1,12 @@
+import {  DataTable } from '@/components/share/data-table';
 import React from 'react'
+
+import data from "./data.json"
 
 export default function Page() {
   return (
     <div>
-      
+      <DataTable data={data} columns={[]} />
     </div>
   )
 }
