077a9dbde50ca9edd2f5eb7942920fdf2cf5308c
YUapisit <aps.apisit250@gmail.com>
2025-06-22 07:34:03 +0700

    Add building route handlers for CRUD operations in admin API

---
+++ src/app/api/admin/building/[id]/route.ts
@@ -0,0 +1,9 @@
+import {
+  UpdateBuilding,
+  GetBuildingById,
+  DeleteBuilding,
+} from '@/controllers/building-controller'
+
+export { GetBuildingById as GET }
+export { UpdateBuilding as PUT }
+export { DeleteBuilding as DELETE }
---
+++ src/app/api/admin/building/route.ts
@@ -0,0 +1,4 @@
+import { CreateBuilding, GetBuildings } from '@/controllers/building-controller'
+
+export { CreateBuilding as POST }
+export { GetBuildings as GET }
